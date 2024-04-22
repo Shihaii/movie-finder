e8e03dcf6bde9dc41422714f265d1dc8d418d7ba
Shihai <Kirill2004val@gmail.com>
2024-04-22 15:17:36 +0300

    made data persistent with pinia in useCurrentMovieStore

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./assets/main.css";
 
 import { createApp } from "vue";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
@@ -12,12 +13,16 @@ import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 const app = createApp(App);
 
-app.use(createPinia());
+
 app.use(router);
 app.use(ElementPlus);
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
 
 app.mount("#app");
--- src/stores/currentMovie.ts
+++ src/stores/currentMovie.ts
@@ -3,23 +3,29 @@ import { defineStore } from "pinia";
 import type { Movie, MovieDetailed } from "@/types";
 import { getMovieById } from "@/controllers/MovieController";
 
-export const useCurrentMovieStore = defineStore("currentMovie", () => {
-  const currentMovie: Ref<Movie> = ref({
-    imdbID: "",
-    title: "",
-    poster: "",
-    year: 0,
-    type: "",
-  });
-  const yearsAfterPublication = computed(
-    () => new Date().getFullYear() - currentMovie.value.year
-  );
-  const getMovieDetailed = async () => {
-    let detailedMovie: MovieDetailed = await getMovieById(
-      currentMovie.value.imdbID
+export const useCurrentMovieStore = defineStore(
+  "currentMovie",
+  () => {
+    const currentMovie: Ref<Movie> = ref({
+      imdbID: "",
+      title: "",
+      poster: "",
+      year: 0,
+      type: "",
+    });
+    const yearsAfterPublication = computed(
+      () => new Date().getFullYear() - currentMovie.value.year
     );
-    return detailedMovie;
-  };
+    const getMovieDetailed = async () => {
+      let detailedMovie: MovieDetailed = await getMovieById(
+        currentMovie.value.imdbID
+      );
+      return detailedMovie;
+    };
 
-  return { currentMovie, yearsAfterPublication, getMovieDetailed };
-});
+    return { currentMovie, yearsAfterPublication, getMovieDetailed };
+  },
+  {
+    persist: true,
+  }
+);
